999601c9b4c7ead8829d8b32e2b5589cb0beb489
sivalien <sivalien@icloud.com>
2024-05-04 20:20:55 +0300

    add task 8

--- project/task3.py
+++ project/task3.py
@@ -14,6 +14,7 @@ from project.task2 import regex_to_dfa, graph_to_nfa
 
 class FiniteAutomaton:
     def __init__(self, fa=None) -> None:
+        self.lbl = True
         self.matrices = {}
         if fa is None:
             self.start_states = set()
@@ -63,14 +64,28 @@ class FiniteAutomaton:
 
         return nfa
 
+    def set_state_to_index(self, d):
+        self.state_to_index = d
+        self.index_to_state = {
+            index: state for state, index in self.state_to_index.items()
+        }
+
+    def set_true(self, label, row, column):
+        self.matrices[label][row, column] = True
+
+    def add_label_if_not_exist(self, label, dim=None):
+        if label not in self.matrices:
+            dim = dim or len(self)
+            self.matrices[label] = dok_matrix((dim, dim), dtype=bool)
+
     def accepts(self, word: Iterable[Symbol]) -> bool:
         return self.to_nfa().accepts(word)
 
     def is_empty(self) -> bool:
-        return len(self.matrices) == 0
+        return self.to_nfa().is_empty()
 
     def get_index(self, state) -> int:
-        return self.state_to_index[state]
+        return self.state_to_index.get(state, 0)
 
     def get_state_by_index(self, index: int):
         return self.index_to_state[index]
@@ -78,8 +93,11 @@ class FiniteAutomaton:
     def __len__(self):
         return len(self.state_to_index)
 
+    def labels(self):
+        return self.state_to_index.keys() if self.lbl else self.matrices.keys()
+
     def get_transitive_closure(self):
-        if self.is_empty():
+        if len(self.matrices.values()) == 0:
             return dok_matrix((0, 0), dtype=bool)
 
         closure = reduce(lambda x, y: x + y, self.matrices.values())
@@ -92,8 +110,9 @@ class FiniteAutomaton:
 
 
 def intersect_automata(
-    automaton1: FiniteAutomaton, automaton2: FiniteAutomaton
+    automaton1: FiniteAutomaton, automaton2: FiniteAutomaton, lbl: bool = True
 ) -> FiniteAutomaton:
+    automaton1.lbl = automaton2.lbl = not lbl
     res = FiniteAutomaton()
 
     for state1, index1 in automaton1.state_to_index.items():
@@ -107,7 +126,7 @@ def intersect_automata(
             if state1 in automaton1.final_states and state2 in automaton2.final_states:
                 res.final_states.add(State(index))
 
-    labels = [label for label in automaton1.matrices if label in automaton2.matrices]
+    labels = automaton1.labels() & automaton2.labels()
     for label in labels:
         res.matrices[label] = kron(
             automaton1.matrices[label], automaton2.matrices[label], "csr"
@@ -118,11 +137,10 @@ def intersect_automata(
 
 def paths_ends(
     graph: MultiDiGraph, start_nodes: set[int], final_nodes: set[int], regex: str
-) -> list[tuple[int, int]]:
-
+) -> list[tuple[object, object]]:
     nfa = FiniteAutomaton(graph_to_nfa(graph, start_nodes, final_nodes))
     dfa = FiniteAutomaton(regex_to_dfa(regex))
-    intersection = intersect_automata(nfa, dfa)
+    intersection = intersect_automata(nfa, dfa, lbl=False)
 
     if intersection.is_empty():
         return []
---
+++ project/task8.py
@@ -0,0 +1,125 @@
+from pyformlang.finite_automaton import State, Symbol
+from pyformlang.cfg import Epsilon, CFG
+from pyformlang.regular_expression import Regex
+from pyformlang.rsa import Box, RecursiveAutomaton
+import networkx as nx
+
+from collections import defaultdict
+
+from project.task2 import graph_to_nfa
+from project.task3 import FiniteAutomaton, intersect_automata
+
+
+def cfg_to_rsm(cfg: CFG) -> RecursiveAutomaton:
+    productions = defaultdict(list)
+
+    for production_item in cfg.productions:
+        if len(production_item.body) == 0:
+            body_regex = Epsilon().to_text()
+        else:
+            body_regex = " ".join(var.value for var in production_item.body)
+        productions[Symbol(production_item.head)].append(body_regex)
+
+    regexes = {
+        Box(Regex("|".join(regex_list)).to_epsilon_nfa().to_deterministic(), symbol)
+        for symbol, regex_list in productions.items()
+    }
+
+    return RecursiveAutomaton(productions.keys(), Symbol("S"), regexes)
+
+
+def ebnf_to_rsm(ebnf: str) -> RecursiveAutomaton:
+    productions = defaultdict(list)
+
+    for line in ebnf.splitlines():
+        line = line.strip()
+        if "->" in line:
+            head, body = line.split("->")
+            body = body.strip() if body.strip() != "" else Epsilon().to_text()
+            productions[Symbol(head.strip())].append(body)
+
+    regexes = {
+        Box(Regex("|".join(regex_list)).to_epsilon_nfa().to_deterministic(), symbol)
+        for symbol, regex_list in productions.items()
+    }
+    return RecursiveAutomaton(productions.keys(), Symbol("S"), regexes)
+
+
+def cfpq_with_tensor(
+    rsm: RecursiveAutomaton,
+    graph: nx.MultiDiGraph,
+    final_nodes: set[int] = None,
+    start_nodes: set[int] = None,
+) -> set[tuple[int, int]]:
+    rsm_matrix = rsm_to_matrix(rsm)[0]
+    automaton = FiniteAutomaton(graph_to_nfa(graph, start_nodes, final_nodes))
+
+    prev_nnz = 0
+    result = set()
+    while True:
+        closure = list(
+            zip(
+                *intersect_automata(rsm_matrix, automaton)
+                .get_transitive_closure()
+                .nonzero()
+            )
+        )
+
+        curr_nnz = len(closure)
+        if curr_nnz == prev_nnz:
+            break
+        prev_nnz = curr_nnz
+
+        for i, j in closure:
+            if (
+                rsm_matrix.get_index(i) in rsm_matrix.start_states
+                and rsm_matrix.get_index(j) in rsm_matrix.final_states
+            ):
+                var = rsm_matrix.indexes_dict()[i].value[0]
+                automaton.add_label_if_not_exist(var)
+                automaton.set_true(var, i, j)
+                result.add((i, j))
+
+    return result
+
+
+def rsm_to_matrix(rsm: RecursiveAutomaton) -> tuple:
+    automaton = FiniteAutomaton()
+    states = set()
+
+    for v, p in rsm.boxes.items():
+        for s in p.dfa.states:
+            states.add(State((v, s.value)))
+
+            if s in p.dfa.start_states:
+                automaton.start_states.add(State((v, s.value)))
+
+            if s in p.dfa.final_states:
+                automaton.final_states.add(State((v, s.value)))
+
+    automaton.set_state_to_index(
+        {
+            value: index
+            for index, value in enumerate(sorted(states, key=lambda x: x.value[1]))
+        }
+    )
+
+    epsilon_symbols = set()
+
+    for v, p in rsm.boxes.items():
+        for src, transition in p.dfa.to_dict().items():
+            for label, dst in transition.items():
+                label = label.value
+
+                if isinstance(dst, Epsilon):
+                    epsilon_symbols.add(label)
+
+                automaton.add_label_if_not_exist(label)
+                for target in {dst} if not isinstance(dst, set) else dst:
+                    automaton.set_true(
+                        label,
+                        automaton.get_index(State((v, src.value))),
+                        automaton.get_index(State((v, target.value))),
+                    )
+
+    return automaton, epsilon_symbols
